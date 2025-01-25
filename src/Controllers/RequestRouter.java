8b372690d664d752b59d503e4d5d8698da745020
Amira Mohamed <amiramohameda745@gmail.com>
2025-01-25 03:35:22 +0200

    Added switch case for GAME_REQUEST + Manually added other team members' code

--- src/Controllers/RequestRouter.java
+++ src/Controllers/RequestRouter.java
@@ -1,6 +1,5 @@
 package Controllers;
 
-
 import org.json.JSONObject;
 
 public class RequestRouter {
@@ -14,16 +13,22 @@ public class RequestRouter {
         String password = jsonReceived.optString("password");
         String email = jsonReceived.optString("email");
         int playerID = jsonReceived.optInt("Player_ID");
-        
+
          RequestHandler requestHandler = new RequestHandler();
       
         switch (requestType) {
             case "SIGN_IN":
-                return requestHandler.signInHandle(username, password); 
+                return requestHandler.signInHandle(username, password, gameClient); 
             case "SIGN_UP":
-                return requestHandler.signUpHandle(username, email ,password) ;
+                return requestHandler.signUpHandle(username, email ,password, gameClient) ;
             case "SIGN_OUT":
                 return requestHandler.signOutHandle(playerID);
+            case "GET_AVAILABLE_PLAYERS":
+                return requestHandler.getAvailablePlayersHandle(playerID);    
+            case "USER_NAME":
+                 return requestHandler.userNameHandle(playerID);  
+            case "GAME_REQUEST":
+                return requestHandler.handleGameRequest(jsonReceived);
             default:
                 return "Error: Invalid request type.";
         }
